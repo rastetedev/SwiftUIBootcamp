596b8c998c3118a39b0f2b81bc5932fac374c8b9
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2024-12-10 13:08:39 -0500

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Raul Astete on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
